eb6396e0b6bae11a0c67a5d37f8956895b6782d4
PawelDabrowski83 <48104564+PawelDabrowski83@users.noreply.github.com>
2021-10-17 08:44:06 +0200

    Update min_max.cpp
    rewrite for C++ conventions

--- min_max.cpp
+++ min_max.cpp
@@ -2,40 +2,39 @@
 #include <vector>
 
 
-void min_max(std::vector<int> &v, int &mn, size_t &in, int &mx, size_t &ix) {
-    int min = v[0];
-    int min_count = 0;
-    int max = v[0];
-    int max_count = 0;
+void min_max(std::vector<int>& v, int& mn, size_t& in, int& mx, size_t& ix) {
+
+    if (v.size() == 0) {
+        return;
+    }
+    mn = v[0];
+    in = 1;
+    mx = v[0];
+    ix = 1;
 
     for (int i = 0; i < v.size(); i++) {
         int current = v[i];
-        if (current == min) {
-            min_count++;
-        } else if (current < min) {
-            min = current;
-            min_count = 1;
+        if (current == mn) {
+            in++;
+        } else if (current < mn) {
+            mn = current;
+            in = 1;
         } 
 
-        if (current == max) {
-            max_count++;
+        if (current == mx) {
+            ix++;
         }
-        else if (current > max) {
-            max = current;
-            max_count = 1;
+        else if (current > mx) {
+            mx = current;
+            ix = 1;
         }
     }
-
-    mn = min;
-    in = min_count;
-    mx = max;
-    ix = max_count;
 }
 
 
 int main()
 {
-    std::vector<int> tab = { 99, 1, 1, 2, 3, 2, 5, 1, 0, 10, 0, 0, 10, 99, 17 };
+    std::vector<int> tab = { 99, 1, 1, 2, 3, 2, 5, 1, 0, 10, 0, 0, 10, 99, 17, -20, 3 };
     int mn = 0;
     size_t in = 0;
     int mx = 0;
