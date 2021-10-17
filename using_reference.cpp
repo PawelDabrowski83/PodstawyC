55eab1d3eeae59e898fa68b4241365966b931428
PawelDabrowski83 <48104564+PawelDabrowski83@users.noreply.github.com>
2021-10-17 08:27:20 +0200

    Create using_reference.cpp

---
+++ using_reference.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 3;
+    int b = 4;
+    swap(a, b);
+
+    std::cout << a << " " << b << "\n";
+
+    int x = 5;
+    std::cout << x << "\n";
+
+    int& y = x; // referencja - alias na nazwę zmiennej 
+    std::cout << y << "\n";
+
+    y = 12;
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    // referencja zmienia wartość adresową w pamięci
+
+    std::cout << &x << "\n"; // adresy zmiennych
+    std::cout << &y << "\n";
+
+}
+
